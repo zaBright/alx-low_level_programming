03cd833ce7e23c0b23d50b2d756a4954c1cce26f
zaBright <linusbright5@gmail.com>
2023-09-13 03:26:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,14 @@ void print_alphabet_x10(void)
 	i = 0;
 
 	while (i < 10)
-
-	for (c = 'a'; c <= 'z'; c++)
 	{
-	_putchar(c);
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+				c++;
+		}
+	_putchar('\n');
 	i++;
 	}
 }
