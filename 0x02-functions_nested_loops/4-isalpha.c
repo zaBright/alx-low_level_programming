8a0510e1379a7d226238d60b99935d4b17d169d0
zaBright <linusbright5@gmail.com>
2023-09-13 04:52:40 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,9 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-
-int_isalpha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-	return (1);
-	else
-		return (0);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
+
