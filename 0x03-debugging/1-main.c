b94a854de58551f1020c5a52c3d2864ed7c35b6d
zaBright <linusbright5@gmail.com>
2023-09-14 08:12:58 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,8 +6,10 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
@@ -15,5 +17,3 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
-
-
