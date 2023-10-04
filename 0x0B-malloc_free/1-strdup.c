a6884d76063214b4e153db4edb84fa9ab1ebc32d
zaBright <linusbright5@gmail.com>
2023-10-04 05:40:40 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,16 @@ char *_strdup(char *str)
 	char *aaa;
 	int i, r = 0;
 
-	if(str == NULL)
-	return (NULL);
+	if (str == NULL)
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
-	i++;
+		i++;
 	aaa = malloc(sizeof(char) * (i + 1));
 	if (aaa == NULL)
-	return (NULL);
+		return (NULL);
+
 	for (r = 0; str[r]; r++)
 	aaa[r] = str[r];
 	return (aaa);
 }
-
